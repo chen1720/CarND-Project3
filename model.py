fad66f1c72ea7d9288050660d4895f30e921838c
chen1720 <chen1720@purdue.edu>
2017-03-03 21:46:51 -0500

    cropping image

--- model.py
+++ model.py
@@ -21,17 +21,24 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
+augmented_images, augmented_measurements = [], []
+for img, measurement in zip(imgs, measurement):
+    augmented_images.append(img)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(img,1))
+    augmented_measurements.append(measurement*-1.0)
 
-X_train = np.array(imgs)
-y_train = np.array(measurements)
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
+from keras.layers import Cropping2D
 from keras.layers.core import Flatten, Dense, Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-
+model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Convolution2D(6, 5, 5, activation="relu"))
 model.add(MaxPooling2D((2, 2)))
