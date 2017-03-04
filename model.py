1f58c3bcddc5bd75ddcf63d0937bdb6699713899
chen1720 <chen1720@purdue.edu>
2017-03-03 22:39:53 -0500

    visualize

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import csv
 import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 lines = []
 with open('driving_log.csv') as csvfile:
@@ -52,6 +54,19 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
 
 model.save('model.h5')
