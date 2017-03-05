908c4d1f4c36f3aaa9b9d49a8312e3d58ace2e6f
chen1720 <chen1720@purdue.edu>
2017-03-05 18:16:35 -0500

    directory update

--- model.py
+++ model.py
@@ -35,27 +35,27 @@ for img, measurement in zip(imgs, measurements):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-# from keras.models import Sequential
-# from keras.layers import Cropping2D
-# from keras.layers.core import Flatten, Dense, Lambda
-# from keras.layers.convolutional import Convolution2D
-# from keras.layers.pooling import MaxPooling2D
-#
-# model = Sequential()
-# model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-# model.add(Cropping2D(cropping=((50,20),(0,0))))
-# model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
-# model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
-# model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
-# model.add(Convolution2D(64, 3, 3, activation="relu"))
-# model.add(Convolution2D(64, 3, 3, activation="relu"))
-# model.add(Flatten())
-# model.add(Dense(100))
-# model.add(Dense(50))
-# model.add(Dense(10))
-# model.add(Dense(1))
-#
-# model.compile(loss='mse', optimizer='adam')
+from keras.models import Sequential
+from keras.layers import Cropping2D
+from keras.layers.core import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20),(0,0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
 #
 # history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2, verbose=1)
 # #
@@ -71,4 +71,4 @@ y_train = np.array(augmented_measurements)
 # # plt.legend(['training set', 'validation set'], loc='upper right')
 # # plt.show()
 #
-# model.save('model.h5')
+model.save('model.h5')
